424bc0c6f8bcb95ed5e740a6cb0901a2d3e987d0
Avilee Williams <avilee.williams@gmail.com>
2022-01-26 12:01:11 -0700

    save

--- src/components/business.vue
+++ src/components/business.vue
@@ -1,13 +1,118 @@
 <template>
-  <p>Business</p>
+  <div>
+
+    <md-card class="md-card-33">
+      <md-card-header>
+        <div class="md-title">Investment Property</div>
+      </md-card-header>
+
+      <md-card-content>
+        <p style="font-size: 4.5mm;">What is an investment property?</p>
+        <p style="font-size: smaller;">An investment property can be any type of rental property that isn’t your primary residence.
+          If you don’t live in a home most of the time, it is an investment property.
+          More specifically, any real estate purchased with the intention of earning a return,
+          usually via rental income or future resale, is an investment property.
+          These can be residential properties, such as single-family homes, townhouses, and apartment buildings,
+          or commercial properties, like restaurants, hotels, and retail space.</p>
+
+      </md-card-content>
+    </md-card>
+
+    <md-card class="md-card-33">
+      <md-card-header>
+        <div class="md-title">Assets</div>
+      </md-card-header>
+
+      <md-card-content>
+       <p>To understand what kind of profits you can expect from your investment property (and to avoid bad investments), you’ll need to do a little math.
+         When deciding how much to charge for rent, consider the 1% rule. This simple calculation takes the price you paid for the property,
+         plus any maintenance costs, and multiplies it by 1%. The number you get from there is a recommendation for how much rent you can charge.
+         (Property price + Repair costs) x 0.01 = Monthly rent
+         Using the 1% rule will typically mean that your rental income meets or exceeds your mortgage payments, but it’s just a starting point.
+         You should also factor in extra costs (routine maintenance, buffer for emergency repairs, and a property manager) before settling on a rate.
+         From there, use the gross rent multiplier (GRM), which is a simple formula that projects how long your rental property will take to pay for itself.
+         Property price / Gross annual rental income = Months until property is paid off
+         This means if you bought a property for $400,000 and set the rent at $4,000 per month (per the 1% rule), the property would
+         theoretically pay for itself in just over eight years.
+       </p>
+      </md-card-content>
+    </md-card>
+
+    <md-card class="md-card-33">
+      <md-card-header>
+        <div class="md-title">Property Management</div>
+      </md-card-header>
+
+      <md-card-content>
+        <p>While collecting rent checks may sound like an easy way to earn income, being a landlord is a huge responsibility that can require a lot of time and energy.
+          Before you purchase an investment property, make sure you understand your state and local landlord-tenant laws. Renters have rights surrounding fair housing,
+          lease requirements, security deposits, rent control, timely repairs, eviction rules, and more.
+          You also need to keep in mind that maintenance and repairs will need to be done regularly,
+          and often very quickly. This means you’ll need to have emergency repair plans, so unexpected issues can be resolved efficiently.
+          If you’re pretty handy with a toolbox and have the time to communicate with renters, it might be feasible to handle most things
+          on your own and call repair people for maintenance beyond your expertise (just as you would with your primary residence).
+          But if you own more than one investment property, don’t live near your investment property, don’t want to do hands-on work,
+          or simply don’t have the time to deal with renters on a regular basis, you might want to consider hiring a property management
+          company. While their services will cut into your profits, they will take a lot of responsibility off your shoulders.
+        </p>
+      </md-card-content>
+    </md-card>
+
+    <md-card class="md-card-33">
+      <md-card-header>
+        <div class="md-title">Background Checks</div>
+      </md-card-header>
+
+      <md-card-content>
+        <p>Before you can run a background check on a tenant, you must obtain written consent and approval
+          from the tenant. It is illegal to run a background check if the renter is unaware you’re doing so.
+          You can include a section in your rental application that explicitly asks for their signature and consent.
+          To run a background check, you need the renter’s name and social security number. Obtain the necessary
+          personal information so you can move forward with the tenant screening process.
+          Before you see the results of a background check, it’s smart to have a game plan in mind on what
+          you’ll do with the information and how it’ll affect your decision. For example, will teenage misdemeanors
+          be a deal-breaker? Or, will you only reject sex offenders, people who’ve been evicted, or hardened criminals?
+          To be a fair landlord, you’ll want to know your boundaries and hold everyone accountable to the same standards,
+          otherwise, you can face discrimination issues in the future.
+          To avoid discrimination and be compliant with the Fair Housing Act, it’s crucial to know the laws and
+          understand what you can and cannot do. For example, you cannot run a background check simply because
+          of someone’s race or gender. Also, you cannot run a background check and use it as a cover for rejecting
+          someone based on sexual orientation or age.</p>
+      </md-card-content>
+    </md-card>
+
+    <md-card class="md-card-33">
+      <md-card-header>
+        <div class="md-title">Location</div>
+      </md-card-header>
+
+      <md-card-content>
+        <p>In real estate, location is king. To understand the areas you might be buying into,
+          do a real estate market analysis. This will help you understand the varying fair
+          market values of different types of properties, determine how much you can charge
+          for rent, and project how well the value of your investment property will appreciate.
+          To do this, you’ll need to research your target area. Consider what kind of amenities,
+          including public transportation, parks, and proximity to good public schools or offices,
+          make your prospective neighborhoods desirable. Then, use multiple sources of reliable
+          data to better understand property values in the area. </p>
+      </md-card-content>
+    </md-card>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: "business"
+  name: "saving"
 }
 </script>
 
-<style scoped>
-
+<style>
+.md-card-33 {
+  width: 32%;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
 </style>
+
